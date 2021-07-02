664c006185102202dc5e69fdc85a677da1ead5cc
Sstark97 <aitorscinfo@gmail.com>
2021-07-02 09:11:38 +0100

    Fix ShopCarousel warnings with image

--- src/containers/ShopCarousel.jsx
+++ src/containers/ShopCarousel.jsx
@@ -3,7 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import Cremas from '../assets/static/cremas.png';
 import Rebajas from '../assets/static/rebajas.png';
-import { Flex, Text, Image} from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 
 const ShopCarousel = () => (
     <Flex width="100%" p="12">
@@ -12,16 +12,16 @@ const ShopCarousel = () => (
 
                 <Flex>
 
-                    <Image src={Cremas} />
+                    <img src={Cremas} alt="Nuevos sticks y cremas solares disponibles"/>
                     <Text className="legend">Nuevos Sticks y Cremas solares</Text>
 
                 </Flex>
 
                 <Flex>
 
-                    <Image src={Rebajas} />
+                    <img src={Rebajas} alt="40% de Rebajas"/>
                     <Text className="legend">40% de Rebajas</Text>
-                    
+
                 </Flex>
 
             </Carousel>
